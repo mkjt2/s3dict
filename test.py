4d15530d604de5b4f8f3ba918e9ba6283e117979
Jackie Tung <jackie.tung@gmail.com>
2024-11-30 22:46:33 -0800

    clean up

--- test.py
+++
@@ -1,9 +0,0 @@
-from s3dict import S3Dict
-import os
-
-os.environ['AWS_ACCESS_KEY_ID'] = 'g48ShmmaEtuyQvopD2p7'
-os.environ['AWS_SECRET_ACCESS_KEY'] = 'wWhCbWHvGhqgn1XrN0trDIgASYwwvV4bwNzrjSkg'
-
-
-s = S3Dict()
-print(s["xfinity_sept.pdf"])
